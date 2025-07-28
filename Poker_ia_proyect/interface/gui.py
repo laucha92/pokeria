e8f8551c61c7632d531bb8b1fd8b27049a8bb50f
laucha92 <laureano.ibargaray@gmail.com>
2025-07-28 19:37:28 -0300

    Delete Poker_ia_proyect directory

--- Poker_ia_proyect/interface/gui.py
+++
@@ -1,7 +0,0 @@
-def start_gui():
-    """
-    Inicia la interfaz gráfica de usuario.
-    """
-    # Implementar la lógica de la interfaz gráfica de usuario aquí
-    # Esto es solo un placeholder
-    pass
--- Poker_ia_proyect/training/generate_data.py
+++
@@ -1,7 +0,0 @@
-def generate_training_data():
-    """
-    Genera datos de entrenamiento.
-    """
-    # Implementar la lógica de generación de datos de entrenamiento aquí
-    # Esto es solo un placeholder
-    pass
